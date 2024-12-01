7459a886c71439ed88467f9e5ea261231c43320d
llookkeesshh00 <llookkeesshh00@gmail.com>
2024-12-01 16:08:29 +0530

    fixingdb error

--- models/connection.js
+++ models/connection.js
@@ -2,11 +2,20 @@ const mongoose = require("mongoose");
 
 const connectionTo = async () => {
   try {
-    console.log('avd');
+    console.log('Attempting to connect...');
     await mongoose.connect('mongodb+srv://llookkeesshh00:inZzxHTVXJiyyspj@cluster0.wtltk.mongodb.net/meatconnect', {
-   
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
     console.log('Connection established');
+
+    // Check if the database exists by inserting a dummy document (only first time)
+    const collection = mongoose.connection.db.collection('meatconnect');
+    await collection.findOneAndUpdate(
+      { _id: 'test' },
+      { $set: { testField: 'test' } },
+      { upsert: true } // Ensures the document is inserted only if it doesn't exist
+    );
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
   }
